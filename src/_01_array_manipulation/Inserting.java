8f7fc62a342d8ea5eec11f03ea0dcfc875bcaaae
Eshaan K <eshaankansagara@gmail.com>
2018-08-31 18:33:42 -0700

    Finish Old Fashioned Arrays
    finish the AddAt() thing

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,7 +1,5 @@
 package _01_array_manipulation;
 
-import java.util.ArrayList;
-
 public class Inserting {
 
 	public static int[] insertAt(int[] testArray, int i, int j) {
@@ -11,39 +9,33 @@ public class Inserting {
 	}
 
 	public static String[] insertAlphabetically(String[] orderedArray, String string) {
-	String[] insert = new String[orderedArray.length + 1] ;
-	
-	
-	for (int i = 0; i < insert.length -1; i++) {
-		for (int j = 0; j < insert.length; j++) {
-			if (orderedArray[i].compareTo(string) < 0) {
-			insert[i] = string;
-		}
-		break;
-		} 
-		if(orderedArray[i].compareTo(string) < 0) {
-			insert[i] = orderedArray[i];
-		}
-	}
+		String[] insert = new String[orderedArray.length + 1];
 
-	
-	for (int i = 0; i < insert.length -1; i++) {
-	System.out.println(insert[i]);
-	}
-	
-	
-//	for (int i = 0; i < insert.length - 1; i++) {
-//	insert[0] = string;
-//	insert[i + 1] = orderedArray[i];
-//	
-//	
-//	System.out.println(insert[i]);
-//	}
-	
-	
-	
-	
-	return insert;
+		// for (int i = 0; i < insert.length -1; i++) {
+		// for (int j = 0; j < insert.length; j++) {
+		// if (orderedArray[i].compareTo(string) < 0) {
+		// insert[i] = string;
+		// }
+		// break;
+		// }
+		// if(orderedArray[i].compareTo(string) < 0) {
+		// insert[i] = orderedArray[i];
+		// }
+		// }
+		//
+		//
+		// for (int i = 0; i < insert.length -1; i++) {
+		// System.out.println(insert[i]);
+		// }
+
+		for (int i = 0; i < insert.length - 1; i++) {
+			insert[0] = string;
+			insert[i + 1] = orderedArray[i];
+
+			System.out.println(insert[i]);
+		}
+
+		return insert;
 	}
 
 }
--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -1,5 +1,44 @@
 package _02_diy_array_list;
 
-public class ArrayList{
+public class ArrayList<T> {
+	// public static void main(String[] args) {
+	// ArrayList<String> h = new ArrayList<>();
+	//
+	// }
+
+	Object[] elements = new Object[10];
+
+	int size = 0;
+
+	int size() {
+		return size;
+	}
+
+	void add(T a) {
+		elements[size] = a;
+		size++;
+	}
+
+	public T get(int g) {
+		return (T) elements[g];
+
+	}
+
+	void set(int i, T s) {
+		elements[i] = s;
+
+	}
+
+	void addAt(int j, T c) {
+
+		for (int i = size; i > 0; i--) {
+			elements[i] = elements[i + 1];
+			if (i == j) {
+				break;
+			}
+		}
+		elements[j] = c;
+		size += 1;
+	}
 
 }
